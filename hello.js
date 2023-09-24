0a063549b1168d0858e1e52ae4478a679995c7a4
vasudev <vasudev65patel@gmail.com>
2023-09-24 20:24:47 +0530

    sumof

--- hello.js
+++ hello.js
@@ -1,1 +1,3 @@
+import {sum} from "./index";
 console.log("Hello World");
+console.log("Sum : ",sum(10)(10));
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const sum=(a)=>{
+export const sum=(a)=>{
     return(b)=>a+b;
 }
-
-console.log(sum(10)(10));
